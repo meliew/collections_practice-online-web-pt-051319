c771c0438012e78b2231d86042c73e531ea91d08
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-21 04:34:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 
+def sort_array_asc(integer_array)
+integer_array.sort do |a, b|
+  a <=> b
+  end
+end
+
+
+def sort_array_desc(integer_array)
+integer_array.sort do |a, b|
+  b <=> a
+  end
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(integers_array)
+  integers_array.reverse
+end
+
+def kesha_maker(array)
+new_array =  array.map do |word|
+  word[2] = "$"
+  word
+  end
+new_array
+end
+
+def find_a(array)
+  array.select do |word|
+  if word.start_with?("a")
+    word
+    end
+  end
+end
+
+def sum_array(numbers_array)
+  sum = 0
+  numbers_array.each do |i|
+    sum += i
+  end
+  sum
+end
+
+def add_s(array)
+  array.map do |element|
+  puts element +'s'
+  end
+end
